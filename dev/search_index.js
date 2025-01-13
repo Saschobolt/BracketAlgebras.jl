497b0b575d18db8b48866aadf3ce95a7e25c196c
Documenter.jl <documenter@juliadocs.github.io>
2025-01-13 13:13:47 +0000

    build based on 34c68a1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BracketAlgebra","category":"page"},{"location":"#BracketAlgebra","page":"Home","title":"BracketAlgebra","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BracketAlgebra.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BracketAlgebra]","category":"page"}]
+}
