172c7b5522bce25810a76af3913b87a9dcf24edd
Sascha Stüttgen <sascha.steuttgen@rwth-aachen.de>
2025-02-05 12:34:30 +0100

    Nemo ring interface in own file

--- src/BracketAlgebras.jl
+++ src/BracketAlgebras.jl
@@ -5,28 +5,28 @@ using Bijections
 using Combinatorics
 
 # Write your package code here.
-abstract type AbstractBracketAlgebra <: Nemo.Ring end
-abstract type AbstractBracketAlgebraElem <: Nemo.RingElem end
+abstract type AbstractBracketAlgebra{T} <: Nemo.Ring end
+abstract type AbstractBracketAlgebraElem{T} <: Nemo.RingElem end
 
-export AbstractBracketAlgebra, BracketAlgebra, sizyges, Tabloid, is_standard, bracket_monomial, reduced_groebner_basis!, AbstractBracketAlgebraElem, BracketAlgebraElem, ordering!, straighten
+export AbstractBracketAlgebra, BracketAlgebra, sizyges, Tabloid, is_standard, bracket_monomial, reduced_groebner_basis!, AbstractBracketAlgebraElem, BracketAlgebraElem, point_ordering!, straighten
 
 
-struct BracketAlgebra{T<:Union{Nemo.RingElem,Number}} <: AbstractBracketAlgebra
-    d::Int # dimension of the modelled projective space $\mathbb{P}^d$
-    n::Int # number of points in the projective space
+mutable struct BracketAlgebra{T<:Nemo.RingElement} <: AbstractBracketAlgebra{T}
+    d::Int # dimension of the underlying projective space $\mathbb{P}^d$
+    n::Int # number of points considered
     R::Nemo.MPolyRing{T}
-    literal_ordering::Vector{Int} # ordering of the n points, that induces the tableaux order. literal_ordering[1] < literal_ordering[2] < ... < literal_ordering[n]. See Sturmfels 2008 p.81
+    point_ordering::Vector{Int} # ordering of the n points, that induces the tableaux order. point_ordering[1] < point_ordering[2] < ... < point_ordering[n]. See Sturmfels 2008 p.81
     variables::Bijection{Vector{Int},<:Nemo.MPolyRingElem{T}}
     # ordering::DegRevLex{<:Nemo.MPolyRingElem{T}}
     # groebner_basis::Union{Nothing,Vector{<:Nemo.MPolyRingElem{T}}}
 
-    function BracketAlgebra(n, d, literal_ordering=collect(1:n), T::Type=Nemo.ZZRingElem)
-        if sort(literal_ordering) != collect(1:n)
-            error("ordering needs to order the literals 1:$n, but got $literal_ordering")
+    function BracketAlgebra(n, d, point_ordering=collect(1:n), T::Type=Nemo.ZZRingElem)
+        if sort(point_ordering) != collect(1:n)
+            error("ordering needs to order the literals 1:$n, but got $point_ordering")
         end
 
         S = Nemo.parent_type(T)
-        brackets = reverse!(sort(sort.(collect(combinations(1:n, d + 1)), lt=_lt(literal_ordering)), lt=_lt(literal_ordering))) # brackets are in the form [a,b,c,...] with a > b > c according to literal_ordering. They are sorted in decreasing order wrt the tableaux order extrapolated from literal_ordering.
+        brackets = reverse!(sort(sort.(collect(combinations(1:n, d + 1)), lt=_lt(point_ordering)), lt=_lt(point_ordering))) # brackets are in the form [a,b,c,...] with a > b > c according to point_ordering. They are sorted in decreasing order wrt the tableaux order extrapolated from literal_ordering.
         vars = Nemo.AbstractAlgebra.variable_names("x#" => brackets)
 
         # the monomial ordering is extrapolated from vars[1] > vars[2] > vars[3]... 
@@ -42,11 +42,52 @@ struct BracketAlgebra{T<:Union{Nemo.RingElem,Number}} <: AbstractBracketAlgebra
 
         # ordering = Groebner.DegRevLex(x)
 
-        return new{Nemo.elem_type(S)}(d, n, R, literal_ordering, variables)
+        return new{Nemo.elem_type(S)}(d, n, R, point_ordering, variables)
     end
 end
 
-mutable struct BracketAlgebraElem{T<:Union{Nemo.RingElem,Number}} <: AbstractBracketAlgebraElem
+
+"""
+    d(B::BracketAlgebra)
+
+Return the dimension of the projective space \$\\mathbb{P}^d\$ underlying the bracket algebra B.
+"""
+d(B::BracketAlgebra) = B.d
+
+"""
+    n(B::BracketAlgebra)
+
+Return the number of points considered in the bracket algebra B.
+"""
+n(B::BracketAlgebra) = B.n
+
+"""
+    point_ordering(B::BracketAlgebra)
+
+Return the ordering of the n points in the bracket algebra B, that induces the tableaux order.
+"""
+point_ordering(B::BracketAlgebra) = B.point_ordering
+
+function point_ordering!(B::BracketAlgebra, point_ordering::AbstractVector{<:Integer}=collect(1:B.n))
+    B.point_ordering = point_ordering
+    brackets = sort(sort.(collect(combinations(1:n(B), d(B) + 1)), lt=_lt(point_ordering)), lt=_lt(point_ordering))
+
+    vars = Nemo.AbstractAlgebra.variable_names("x#" => brackets)
+    B.R, x = Nemo.polynomial_ring(Nemo.base_ring(B.R), vars; internal_ordering=:degrevlex)
+    B.variables = Bijection{Vector{Int},typeof(x[1])}()
+
+    for (i, bracket) in enumerate(brackets)
+        B.variables[bracket] = x[i]
+    end
+
+    return B
+end
+
+function Base.show(io::IO, B::BracketAlgebra{T}) where {T}
+    print(io, "Bracket algebra over P^$(d(B)) on $(n(B)) points.")
+end
+
+mutable struct BracketAlgebraElem{T<:Union{Nemo.RingElem,Number}} <: AbstractBracketAlgebraElem{T}
     parent::BracketAlgebra{T}
     polynomial::Nemo.MPolyRingElem{T}
 end
@@ -82,6 +123,7 @@ function Base.show(io::IO, b::BracketAlgebraElem)
     print(io, str)
 end
 
+include("ring_interface.jl")
 include("basics.jl")
 
 end
